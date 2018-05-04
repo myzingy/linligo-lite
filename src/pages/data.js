cb5dbd97add0dea708bf3b61dc19a22f6d4f8142
wangfan <fan.wang@colorcun.com>
2018-05-04 18:54:31 +0800

    first

---
+++ src/pages/data.js
@@ -0,0 +1,9 @@
+/**
+ * Created by goto9 on 2018/5/4.
+ */
+export default {
+    //'楼下自提','送货到家','来我家取'
+    distribution:[{name:'楼下自提'},{name:'送货到家'},{name:'来我家取'}],
+    //'蔬菜','水果','肉蛋','水产'
+    types:[{name:'蔬菜'},{name:'水果'},{name:'肉蛋'},{name:'水产'}],
+}
