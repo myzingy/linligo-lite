8cc5fd56f95a6fd07a36df9cc87d0ded10dfd0dc
wangfan <fan.wang@colorcun.com>
2018-05-06 00:57:55 +0800

    first

--- src/pages/data.js
+++ src/pages/data.js
@@ -6,4 +6,17 @@ export default {
     distribution:[{name:'楼下自提'},{name:'送货到家'},{name:'来我家取'}],
     //'蔬菜','水果','肉蛋','水产'
     types:[{name:'蔬菜'},{name:'水果'},{name:'肉蛋'},{name:'水产'}],
+    //activity status
+    activityStatus:{
+        0:'待采购',
+        1:'采购中',
+        2:'待派送',
+
+        3:'派送中',
+        4:'派送完',
+
+        10:'已取消',
+        11:'已拒绝',
+        12:'缺货',
+    },
 }
--- src/util/uri.js
+++ src/util/uri.js
@@ -2,11 +2,18 @@ export default {
     isProduction:function(){
         return process.env.NODE_ENV === 'production';
     },
-    cgi:function(uri=""){
+    cgi:function(uri="",data={}){
         var base_url=this.isProduction()?"https://linli.vking.wang/api":"http://www.linligo.com/api";
         if(typeof uri =='string') return base_url.replace('api','')+uri;
         base_url+='/'+uri.act;
         console.log('isProduction',this.isProduction(),base_url);
+        if(base_url.indexOf('{')>-1){
+            base_url=base_url.replace(/\{([^\}]+)\}/g,function($0,$1){
+                console.log('replace',arguments);
+                return data[$1] || "";
+            });
+        }
+        base_url=base_url.replace(/\/+$/,'');
         return base_url;
     },
     user:{act:'user',code:'10000',type:'GET',timeout:0},
@@ -15,4 +22,5 @@ export default {
     setPhoneNumber:{act:'wxuser/setPhoneNumber',code:'10003',type:'POST',timeout:0,silence:true},
 
     purchaseCreate:{act:'activity',code:'10004',type:'POST',timeout:0},
+    purchase:{act:'activity/{id}',code:'10005',type:'GET',timeout:0},
 }
